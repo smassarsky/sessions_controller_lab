09e26d7f57b016e77e54acb3df7e312cd7e587e6
smassarsky <seth.massarsky@gmail.com>
2020-12-08 15:32:39 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login', alert: "No name provided"
+    end
+  end
+
+  def destroy
+    session.clear
+  end
+
+end
